e582782694aaebc6e6806b28dd1aec8bb1b7246e
kalatea <hanna.komorzycka@gmail.com>
2020-06-26 12:00:54 +0200

    Add new files

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Home = () => (
+  <div>
+    <h2>Hello World</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget posuere eros, nec lobortis eros. Aenean porttitor efficitur ligula. Nulla sodales arcu augue, at interdum velit fringilla a. Donec urna mauris, interdum ac porta sit amet, elementum id tellus. Integer dapibus volutpat consectetur. Quisque nec fermentum velit. Cras ut imperdiet odio, id semper nisl.</p>
+    <p>Sed egestas ligula ac nibh interdum, eget laoreet massa dapibus. Duis placerat eros magna, ac viverra orci volutpat ac. Sed eget arcu non dui placerat gravida vel lacinia quam. Aenean tincidunt neque quis leo feugiat feugiat. Nulla leo nulla, ullamcorper eu iaculis sed, semper a dui. Aenean eu ultricies lorem, eget consectetur metus. Nullam venenatis pharetra velit, vitae feugiat risus. Pellentesque faucibus vulputate mi, id eleifend eros tincidunt vestibulum. Mauris placerat ullamcorper ante. Sed porta sodales metus et condimentum. Aenean placerat nibh eu laoreet tempor. Etiam vel pellentesque nisi. Maecenas in sapien enim. Nam a consectetur lorem.</p>
+    <p>Sed iaculis, quam eget viverra volutpat, diam ex suscipit ligula, ac porta sapien ex et nibh. Duis vehicula non augue eget elementum. Donec eget ullamcorper lorem. Sed ut ligula vitae est hendrerit tincidunt. Morbi luctus diam sed nisi aliquam varius. Sed dictum lorem dolor, at gravida diam ultrices id. Proin ac mauris condimentum nisi auctor fermentum ut a nibh. Sed semper pellentesque volutpat.</p>
+  </div>
+)
